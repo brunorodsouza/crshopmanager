b194526aa0e71d28ad27b465663bcde58509c903
VitielloL <lucasvitielloo@gmail.com>
2022-04-19 01:19:12 -0300

    feat: login template

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,2 +1,27 @@
 @extends('layout')
 
+@section('conteudo')
+<body id="fundoLogin">
+    <div class="card telaLogin" id="telaLogin">
+        <div class="card-body">
+            <img src={{asset("storage/download-removebg-preview.png")}}>
+            <h2>Bar do Francês</h2>
+            <form>
+                <div class="mb-3">
+                    <input type="email" class="form-control modificaInput" id="" aria-describedby="emailHelp" placeholder="Email" required>
+                </div>
+
+                <div class="mb-3">
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <button type="submit" class="btn botaoLogin" style="width: 85%">Entrar</button>
+            </form>
+            <div class="wrapLink">
+                <a class="underlineHover" href="#">Esqueceu sua senha?</a>
+                <a class="underlineHover" href="#">Novo por aqui? Cadastre-se</a>
+            </div>
+        </div>
+    </div>
+    </body>
+    @endsection
