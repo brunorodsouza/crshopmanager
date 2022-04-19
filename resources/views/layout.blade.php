d043f80354515bb1dbdd11c2e0ff45417715da9e
VitielloL <lucasvitielloo@gmail.com>
2022-04-19 01:06:45 -0300

    feat: route login

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>crshopmanager</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href={{asset('css/login/login.css')}}>
+</head>
+
+<body>
+    @yield('conteudo')
+
+</body>
+
+</html>
