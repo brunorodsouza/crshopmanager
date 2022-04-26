f39bab91aa48af47ca1ec8c3d75117173f65ec8f
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-04-25 23:07:21 -0300

    feat: table default and seed example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'administrador',
+            'email' => 'administrador',
+            'password' => bcrypt('demo')
+        ]);
     }
+
 }
