c540293180c02922d38cc9f0bf8056b980895c87
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-04-29 11:30:17 -0300

    feat: ajust routes login

--- resources/views/login/registrar.blade.php
+++ resources/views/login/registrar.blade.php
@@ -22,7 +22,7 @@
                 <button type="submit" class="btn botaoLogin" style="width: 85%">Cadastrar</button>
             </form>
             <div class="wrapLink">
-                <a class="underlineHover" href='{{route('esqueceusenha')}}'s>Esqueceu sua senha?</a>
+                <a class="underlineHover" href='{{route('esqueceusenha')}}'>Esqueceu sua senha?</a>
             </div>
         </div>
     </div>
