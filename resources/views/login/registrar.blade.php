b2548895b657eef563c048a51fdf602466164e5d
Bruno Rodrigues Souza <bruno.rodriguesouza1@gmail.com>
2022-05-10 17:34:49 -0300

    Criando Registrar Usuário
    Comecei a produção do formulário de criação de usuário para login no software.

--- resources/views/login/registrar.blade.php
+++ resources/views/login/registrar.blade.php
@@ -1,14 +1,70 @@
 @extends('layout')
 
 @section('conteudo')
-<body id="fundoLogin">
-    <div class="card telaLogin" id="telaLogin">
+<body>
+    <div class="card telaLogin" id="telaRegistrar">
         <div class="card-body">
-            <img src={{asset("storage/download-removebg-preview.png")}}>
-            <h2>Bar do Francês</h2>
+            <!--<img src={{asset("storage/download-removebg-preview.png")}}>-->
+            <h2>Registro de Usuário</h2>
             <form>
                 <div class="mb-3">
-                    <input type="email" class="form-control modificaInput" id="" aria-describedby="emailHelp" placeholder="Email" required>
+                    <span class="input-group-text">Matrícula</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Nome Completo</span>
+                    <input type="text" class="form-control modificaInput" id="" aria-describedby="emailHelp" placeholder="Nome Completo" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">E-mail</span>
+                    <input type="email" class="form-control modificaInput" id="" placeholder="E-mail" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Data de Nascimento</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">CPF</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Telefone</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Celular</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">CEP</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Logradouro</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Bairro</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Cidade</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
+                </div>
+
+                <div class="mb-3">
+                    <span class="input-group-text">Estado</span>
+                    <input type="password" class="form-control modificaInput" id="" placeholder="Senha" required>
                 </div>
 
                 <div class="mb-3">
