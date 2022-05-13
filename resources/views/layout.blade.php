3db85488fdb819202d534fa93021449895e05834
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-05-13 13:06:45 -0300

    feat: correcao de telas

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,24 +1,16 @@
-<!doctype html>
 <html lang="pt-br">
 <head>
-    <!--Meta Tags-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <!--Título-->
     <title>C. R. Shop Manager</title>
-
-    <!--CSS-->
-    <link rel="stylesheet" href="{{asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href={{asset('css/login/login.css')}}>
 </head>
 
 <body>
     @yield('conteudo')
 
-    <!--JavaScript-->
-    <script type="text/javascript" href="{{asset('js/app.js') }}">
 </body>
 
 </html>
