c3cd45886ee0f8652c71ec2e93b99deea2be9c6b
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-05-13 17:57:39 -0300

    feat: correcao layout

--- resources/views/layout2.blade.php
+++ resources/views/layout2.blade.php
@@ -6,6 +6,7 @@
     <title>C. R. Shop Manager</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href={{asset('css/bootstrap/bootstrap.min.css')}}>
+
 </head>
 
 <body>
