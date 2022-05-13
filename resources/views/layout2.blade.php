f74841523c113b0c86d06337d567b3c98be2fe58
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-05-13 17:54:28 -0300

    feat: bootstrap layout2

--- resources/views/layout2.blade.php
+++ resources/views/layout2.blade.php
@@ -5,9 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>C. R. Shop Manager</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    {{-- <link rel="stylesheet" type="text/css" href={{asset('css/login/login.css')}}>
-    <link rel="stylesheet" type="text/css" href={{asset('css/navbar.css')}}> --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href={{asset('css/bootstrap/bootstrap.min.css')}}>
 </head>
 
 <body>
