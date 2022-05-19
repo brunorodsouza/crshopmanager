8f0650acc41bea1b16d889ef2d6ff85cf7f6f29e
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-05-18 23:00:22 -0300

    feat: add layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>C. R. Shop Manager</title>
+
+        <!--CSS-->
+        <link rel="stylesheet" type="text/css" href={{asset('css/bootstrap/bootstrap.min.css')}}>
+        <link rel="stylesheet" type="text/css" href={{asset('css/app.css')}}>
+        <link rel="stylesheet" type="text/css" href={{asset('css/login/login.css')}}>
+        <link rel="stylesheet" type="text/css" href={{asset('css/login/registrar.css')}}>
+        <!--FONTE UBUNTU-->
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        @yield('conteudo') <!--Chama section 'conteudo'-->
+        @yield('scripts') <!--Chama section 'scripts'-->
+    </body>
+</html>
