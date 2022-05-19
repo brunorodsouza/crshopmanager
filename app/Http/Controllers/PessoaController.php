38313887d3fdd8cd9c1b296dc4bc1eb552a1a649
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-05-18 23:58:22 -0300

    feat: create pessoa

--- app/Http/Controllers/PessoaController.php
+++ app/Http/Controllers/PessoaController.php
@@ -32,24 +32,24 @@ class PessoaController extends Controller
 
     public function store(Request $request)
     {
-        $dado = [
-            'nome' => $request-> nome,
-            'matricula' => $request-> matricula,
-            'cpf' => $request-> cpf,
-            'email' => $request-> email,
-            'data_nascimento' => $request-> data_nascimento,
-            'telefone' => $request-> telefone,
-            'celular' => $request-> celular,
-            'cep' => $request-> cep,
-            'uf' => $request-> uf,
-            'cidade' => $request-> cidade,
-            'bairro' => $request-> bairro,
-            'logradouro' => $request-> logradouro,
-            'numero' => $request-> numero,
-            'complemento' => $request-> complemento,
-        ];
-        Pessoa::create($dado);
-
+        $dados = $request->all();
+        // $dado = [
+        //     'nome' => $request-> nome,
+        //     'matricula' => $request-> matricula,
+        //     'cpf' => $request-> cpf,
+        //     'email' => $request-> email,
+        //     'data_nascimento' => $request-> data_nascimento,
+        //     'telefone' => $request-> telefone,
+        //     'celular' => $request-> celular,
+        //     'cep' => $request-> cep,
+        //     'uf' => $request-> uf,
+        //     'cidade' => $request-> cidade,
+        //     'bairro' => $request-> bairro,
+        //     'logradouro' => $request-> logradouro,
+        //     'numero' => $request-> numero,
+        //     'complemento' => $request-> complemento,
+        // ];
+        Pessoa::create($dados);
         return redirect()->route('pessoa');
     }
 
--- resources/views/pessoa/create.blade.php
+++ resources/views/pessoa/create.blade.php
@@ -4,43 +4,44 @@
 <div class="container my-4">
     <h1> CRIANDO VAGABUNDOS que frequentam o bar do frances </h1>
 
-    <form action="<?= url('/pessoa/store');?>" method="post">
+    <form action='{{route('pessoa.store')}}' method="POST">
+        @csrf
         <p class="text-center bg-secondary text-white fw-bold">Dados Cadastrais</p>
 
         <div class="form-group row">
             <div class="col-md-6">
                 <label class="form-label" for="nome">Nome:</label>
-                <input type="text" class="form-control" id="nome">
+                <input type="text" class="form-control" id="nome" name="nome">
             </div>
             <div class="col-md-6">
                 <label class="form-label" for="cpf">CPF:</label>
-                <input type="text" class="form-control" id="cpf" >
+                <input type="text" class="form-control" id="cpf" name="cpf" >
             </div>
         </div>
 
         <div class="form-group row mt-2">
             <div class="col-md-6">
                 <label for="matricula" class="form-label">Matrícula:</label>
-                <input type="text" class="form-control" id="matricula" >
+                <input type="text" class="form-control" id="matricula" name="matricula">
             </div>
             <div class="col-md-3">
                 <label  class="form-label" for="data_nascimento">Data Nascimento:</label>
-                <input type="date" class="form-control" id="data_nascimento" >
+                <input type="date" class="form-control" id="data_nascimento" name="data_nascimento" >
             </div>
             <div class="col-md-3">
                 <label class="form-label" for="telefone">Telefone:</label>
-                <input type="text" class="form-control" id ="" >
+                <input type="text" class="form-control" id ="" name="telefone" >
             </div>
         </div>
 
         <div class="form-group row my-2">
             <div class="col-md-3">
                 <label  class="form-label" for="celular">Celular:</label>
-                <input type="text" class="form-control" id="celular">
+                <input type="text" class="form-control" id="celular" name="celular">
             </div>
             <div class="col-md-5">
                 <label  class="form-label" for="email">E-mail:</label>
-                <input type="text" class="form-control" id ="email" >
+                <input type="text" class="form-control" id ="email" name="email">
             </div>
         </div>
 
@@ -49,16 +50,16 @@
         <div class="form-group row">
             <div class="col-md-4">
                 <label class="form-label" for="cep">CEP:</label>
-                <input type="text" class="form-control" id="cep">
+                <input type="text" class="form-control" id="cep" name="cep">
             </div>
             <div class="col-md-5">
                 <label class="form-label" for="bairro">Bairro:</label>
-                <input type="text" class="form-control" id ="bairro" >
+                <input type="text" class="form-control" id ="bairro" name="bairro">
             </div>
             <div class="col-md-2">
                 <div class="form-group row">
                     <label class="form-label" for="numero">Nº:</label>
-                    <input type="text" class="form-control" id ="numero" >
+                    <input type="text" class="form-control" id ="numero" name="numero">
                 </div>
             </div>
         </div>
@@ -66,22 +67,22 @@
         <div class="form-group row mt-2">
             <div class="col-md-6">
                 <label class="form-label" for="logradouro">Logradouro:</label>
-                <input type="text" class="form-control" id="logradouro" >
+                <input type="text" class="form-control" id="logradouro" name="logradouro" >
             </div>
             <div class="col-md-6">
                 <label class="form-label" for="complemento">Complemento:</label>
-                <input type="text" class="form-control" id ="complemento" >
+                <input type="text" class="form-control" id ="complemento" name="complemento">
             </div>
         </div>
 
         <div class="form-group row mt-2">
             <div class="col-md-6">
                 <label class="form-label" for="cidade">Cidade:</label>
-                <input type="text" class="form-control" id="cidade" >
+                <input type="text" class="form-control" id="cidade" name="cidade">
             </div>
             <div class="col-md-2">
                 <label class="form-label" for="uf">UF:</label>
-                <input type="text" class="form-control" id ="uf">
+                <input type="text" class="form-control" id ="uf" name="uf">
             </div>
         </div>
 
