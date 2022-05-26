b633997b6e161f2559a4bcd1514772659146d9e7
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-05-26 16:13:45 -0300

    refactor: limpeza de codigo nos models

--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -7,21 +7,6 @@ use Illuminate\Support\Facades\Date;
 
 class Pessoa extends Model
 {
-    // private $matricula;
-    // private $cpf;
-    // private $nome;
-    // private Date $data_nascimento;
-    // private $telefone;
-    // private $celular;
-    // private $email;
-    // private $cep;
-    // private $bairro;
-    // private $logradouro;
-    // private $numero;
-    // private $complemento;
-    // private $cidade;
-    // private $uf;
-
     protected $table = 'pessoas';
 
     protected $fillable = [
@@ -41,8 +26,6 @@ class Pessoa extends Model
         'complemento',
         'tipoStatus',
     ];
-
-
 
     public function mecanico()
     {
