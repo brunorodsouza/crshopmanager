befabbd2d2501a78df87d3c806d7210e32d502a4
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-06-07 15:41:11 -0300

    refactor: identacao seed ordem servico

--- database/seeders/popula_tabela_ordem_servico.php
+++ database/seeders/popula_tabela_ordem_servico.php
@@ -11,21 +11,17 @@ class popula_tabela_ordem_servico extends Seeder
         $servicoId = DB::statement('SELECT TOP 1 id FROM servico ');
         $veiculoId = DB::statement('SELECT TOP 1 id FROM veiculo ');
         $pessoaId = DB::statement('SELECT TOP 1 id FROM pessoas ');
-        
-        
+
         DB::table('ordem_servico')->insert([
-            
             'data_inicio' => '2022-06-12',
             'data_previsao' => '2022-06-12',
             'data_fim' => '2022-06-13',
             'valor_total_material' => '70.00',
             'valor_servico' => '270.00',
             'valor_pago' => '270.00',
-            
             'id_pessoa' => '3',
             'id_veiculo' => '1',
             'id_servico' => '3',
-            
             'status_pagamento' => 1,
         ]);
     }
