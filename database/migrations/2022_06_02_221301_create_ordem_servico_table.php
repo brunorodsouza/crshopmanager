d18f4e02878401ec345e65b073ed28cc70baa8d3
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-06-07 15:39:18 -0300

    refactor: identacao migrate ordem servico

--- database/migrations/2022_06_02_221301_create_ordem_servico_table.php
+++ database/migrations/2022_06_02_221301_create_ordem_servico_table.php
@@ -6,27 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateOrdemServicoTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('ordem_servico', function (Blueprint $table) {
             $table->increments('id');
-            
-            
-            
             $table->date('data_inicio');
             $table->date('data_previsao');
             $table->date('data_fim');
             $table->decimal('valor_total_material');
             $table->decimal('valor_servico');
             $table->decimal('valor_pago');
-            
-            
-            //identificação do cliente 
+            //identificação do cliente
             $table->integer('id_pessoa')->unsigned();
             $table->foreign('id_pessoa')->references('id')->on('pessoas');
             //identifição do veiculo
@@ -35,17 +25,11 @@ class CreateOrdemServicoTable extends Migration
             //identificação do servico
             $table->integer('id_servico')->unsigned();
             $table->foreign('id_servico')->references('id')->on('servico');
-            
             $table->boolean('status_pagamento');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('ordem_servico');
