e5bc9a5952c1966598416342f400304b13e8c350
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-07-13 11:16:11 -0300

    feat: melhoria no create e edit veiculo

--- resources/views/veiculo/create.blade.php
+++ resources/views/veiculo/create.blade.php
@@ -21,39 +21,71 @@
                     </select>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <label class="form-label" for="modelo">Modelo: </label>
-                    <input type="text" class="form-control" id="modelo" name="modelo">
+                    <select class="custom-select custom-select-md mb-3" id="modelo" name="modelo">
+                        <option selected>Selecione o Modelo</option>
+                        <option value='Carro Conversível'>Carro Conversível</option>
+                        <option value='Carro Sedã'>Carro Sedã</option>
+                        <option value='Carro Hatch'>Carro Hatch</option>
+                        <option value='Coupé ou Cupê (Fastback)'>Coupé ou Cupê (Fastback)</option>
+                        <option value='Perua ou SW (Station Wagons)'>Perua ou SW (Station Wagons)</option>
+                        <option value='SUV ou Veículo Utilitário Esportivo'>SUV ou Veículo Utilitário Esportivo</option>
+                        <option value='Picape'>Picape</option>
+                        <option value='Minivan'>Minivan</option>
+                        <option value='Van ou utilitário'>Van ou utilitário</option>
+                        <option value='Buggy ou bugre'>Buggy ou bugre</option>
+                    </select>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <label class="form-label" for="marca">Marca: </label>
-                    <input type="text" class="form-control" id="marca" name ="marca">
-                </div>
-
-                <div class="col-md-2">
-                    <label class="form-label" for="cor">Cor: </label>
-                    <input type="text" class="form-control" id="cor" name="cor">
+                    <select class="custom-select custom-select-md mb-3" id="marca" name ="marca">
+                        <option selected>Selecione a Marca</option>
+                        <option value='Ford'>Ford</option>
+                        <option value='Chevrolet'>Chevrolet</option>
+                        <option value='Toyota'>Toyota</option>
+                        <option value='BMW'>BMW</option>
+                        <option value='Volkswagen'>Volkswagen</option>
+                        <option value='Ferrari'>Ferrari</option>
+                        <option value='Hyundai'>Hyundai</option>
+                        <option value='Honda'>Honda</option>
+                        <option value='Land Rover'>Land Rover</option>
+                    </select>
                 </div>
             </div>
 
             <div class="form-group row">
                 <div class="col-md-3">
+                    <label class="form-label" for="cor">Cor: </label>
+                    <input type="text" class="form-control" id="cor" name="cor">
+                </div>
+
+                <div class="col-md-2">
                     <label class="form-label" for="ano_fab">Ano Fabricação: </label>
-                    <input type="text" class="form-control" id="ano_fab" name="ano_fab">
+                    <input type="number" class="form-control" minlength="4" maxlength="4" id="ano_fab" name="ano_fab">
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <label class="form-label" for="ano_mod">Ano Modelo: </label>
-                    <input type="text" class="form-control" id="ano_mod" name="ano_mod">
+                    <input type="number" class="form-control" minlength="4" maxlength="4" id="ano_mod" name="ano_mod">
                 </div>
 
                 <div class="col-md-3">
-                    <label class="form-label" for="combustivel">Combustivel: </label>
-                    <input type="text" class="form-control" id="combustivel" name="combustivel">
+                    <label class="form-label" for="combustivel">Combustível: </label>
+                    <select class="custom-select custom-select-md mb-3" name="combustivel" id="combustivel">
+                        <option selected>Selecione o Combustível</option>
+                        <option value='Gasolina aditivada'>Gasolina aditivada</option>
+                        <option value='Gasolina premium'>Gasolina premium</option>
+                        <option value='Gasolina formulada'>Gasolina formulada</option>
+                        <option value='Etanol'>Etanol</option>
+                        <option value='Etanol aditivado'>Etanol aditivado</option>
+                        <option value='GNV (Gás Natural Veicular)'>GNV (Gás Natural Veicular)</option>
+                        <option value='Diesel'>Diesel</option>
+                    </select>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <label class="form-label" for="placa">Placa: </label>
                     <input type="text" class="form-control" id="placa" name ="placa">
                 </div>
--- resources/views/veiculo/edit.blade.php
+++ resources/views/veiculo/edit.blade.php
@@ -14,45 +14,77 @@
             <p class="text-center bg-secondary text-white fw-bold">Dados Veículo</p>
 
             <div class="form-group row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <label class="form-label" for="id_pessoa">Selecione o Cliente: </label>
                         <input type="text" class="form-control" id="modelo" value="{{$dado->pessoas->nome}}" disabled>
                     </select>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <label class="form-label" for="modelo">Modelo: </label>
-                    <input type="text" class="form-control" id="modelo" name="modelo" value="{{$dado->modelo}}">
+                    <select class="custom-select custom-select-md mb-3" id="modelo" name="modelo">
+                        <option value="{{$dado->modelo}}" selected>{{$dado->modelo}}</option>
+                        <option value='Carro Conversível'>Carro Conversível</option>
+                        <option value='Carro Sedã'>Carro Sedã</option>
+                        <option value='Carro Hatch'>Carro Hatch</option>
+                        <option value='Coupé ou Cupê (Fastback)'>Coupé ou Cupê (Fastback)</option>
+                        <option value='Perua ou SW (Station Wagons)'>Perua ou SW (Station Wagons)</option>
+                        <option value='SUV ou Veículo Utilitário Esportivo'>SUV ou Veículo Utilitário Esportivo</option>
+                        <option value='Picape'>Picape</option>
+                        <option value='Minivan'>Minivan</option>
+                        <option value='Van ou utilitário'>Van ou utilitário</option>
+                        <option value='Buggy ou bugre'>Buggy ou bugre</option>
+                    </select>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <label class="form-label" for="marca">Marca: </label>
-                    <input type="text" class="form-control" id="marca" name ="marca" value="{{$dado->marca}}">
-                </div>
-
-                <div class="col-md-3">
-                    <label class="form-label" for="cor">Cor: </label>
-                    <input type="text" class="form-control" id="cor" name="cor" value="{{$dado->cor}}">
+                    <select class="custom-select custom-select-md mb-3" id="marca" name ="marca">
+                        <option value="{{$dado->marca}}" selected>{{$dado->marca}}</option>
+                        <option value='Ford'>Ford</option>
+                        <option value='Chevrolet'>Chevrolet</option>
+                        <option value='Toyota'>Toyota</option>
+                        <option value='BMW'>BMW</option>
+                        <option value='Volkswagen'>Volkswagen</option>
+                        <option value='Ferrari'>Ferrari</option>
+                        <option value='Hyundai'>Hyundai</option>
+                        <option value='Honda'>Honda</option>
+                        <option value='Land Rover'>Land Rover</option>
+                    </select>
                 </div>
             </div>
 
             <div class="form-group row">
                 <div class="col-md-3">
-                    <label class="form-label" for="ano_fab">Ano Fabricação: </label>
-                    <input type="text" class="form-control" id="ano_fab" name="ano_fab" value="{{$dado->ano_fab}}">
+                    <label class="form-label" for="cor">Cor: </label>
+                    <input type="text" class="form-control" id="cor" name="cor" value="{{$dado->cor}}">
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-2">
+                    <label class="form-label" for="ano_fab">Ano Fabricação: </label>
+                    <input type="number" class="form-control" minlength="4" maxlength="4" id="ano_fab" name="ano_fab" value="{{$dado->ano_fab}}">
+                </div>
+
+                <div class="col-md-2">
                     <label class="form-label" for="ano_mod">Ano Modelo: </label>
-                    <input type="text" class="form-control" id="ano_mod" name="ano_mod" value="{{$dado->ano_mod}}">
+                    <input type="number" class="form-control" minlength="4" maxlength="4" id="ano_mod" name="ano_mod" value="{{$dado->ano_mod}}">
                 </div>
 
                 <div class="col-md-3">
                     <label class="form-label" for="combustivel">Combustivel: </label>
-                    <input type="text" class="form-control" id="combustivel" name="combustivel" value="{{$dado->combustivel}}">
+                    <select class="custom-select custom-select-md mb-3" name="combustivel" id="combustivel">
+                        <option value="{{$dado->combustivel}}" selected>{{$dado->combustivel}}</option>
+                        <option value='Gasolina aditivada'>Gasolina aditivada</option>
+                        <option value='Gasolina premium'>Gasolina premium</option>
+                        <option value='Gasolina formulada'>Gasolina formulada</option>
+                        <option value='Etanol'>Etanol</option>
+                        <option value='Etanol aditivado'>Etanol aditivado</option>
+                        <option value='GNV (Gás Natural Veicular)'>GNV (Gás Natural Veicular)</option>
+                        <option value='Diesel'>Diesel</option>
+                    </select>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <label class="form-label" for="placa">Placa: </label>
                     <input type="text" class="form-control" id="placa" name ="placa" value="{{$dado->placa}}">
                 </div>
